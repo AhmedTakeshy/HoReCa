9e25c412987e2f9554dd1dbf0cee0e2921e7e6ec
Ahmed Takeshy <ahmedtakeshy7@gmail.com>
2023-12-26 12:48:39 +0300

    Add NavMenu component

---
+++ src/components/home/NavMenu.tsx
@@ -0,0 +1,98 @@
+"use client"
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import Link from "next/link"
+import { ModeToggler } from "../shared/ModeToggler"
+import Image from "next/image"
+import { useEffect, useState } from "react"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
+import { Button } from "../ui/button"
+
+
+export default function NavMenu() {
+    const [showBg, setShowBg] = useState(false)
+    const [open, setOpen] = useState<boolean>(false)
+
+    return (
+        <header className={`flex justify-between items-center`}>
+            <NavigationMenu className={`!container items-center rounded-xl max-w-none px-8 justify-between hidden gap-2 md:flex h-full`}>
+                <Image className="ml-auto" src="/imgs/logo.png" width={100} height={75} alt="logo" />
+                <NavigationMenuList className="items-center justify-between hidden gap-2 md:flex ">
+                    <NavigationMenuItem>
+                        <Link href="/" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>الرئيسية</NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/#services" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>الخدمات</NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/#testimonials" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>العملاء</NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/#about" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>من نحن</NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <ModeToggler />
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+            {/* Nav for small screens */}
+            <div className={`flex items-center justify-between h-full md:hidden rounded-xl px-8 my-8 `}>
+                <Image className="ml-auto" src="/imgs/logo.png" width={130} height={32} alt="logo" />
+                <Popover onOpenChange={setOpen} open={open}>
+                    <PopoverTrigger aria-controls="2" aria-labelledby="open menu button" asChild>
+                        <div className="flex items-center gap-2">
+                            <ModeToggler />
+                            <Button aria-description="open main menu" aria-label="open menu" data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                                <span className="sr-only">Open main menu</span>
+                                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                                </svg>
+                            </Button>
+                        </div>
+                    </PopoverTrigger>
+                    <PopoverContent className="relative w-screen ">
+                        <div className="flex flex-col items-center mx-auto md:flex-row">
+                            <NavigationMenu>
+                                <NavigationMenuList className="flex flex-col items-center justify-center gap-2">
+                                    <NavigationMenuItem>
+                                        <Link href="/" legacyBehavior passHref>
+                                            <NavigationMenuLink onClick={() => setOpen(false)} className={navigationMenuTriggerStyle()}>الرئيسية</NavigationMenuLink>
+                                        </Link>
+                                    </NavigationMenuItem>
+                                    <NavigationMenuItem>
+                                        <Link href="/#services" legacyBehavior passHref>
+                                            <NavigationMenuLink onClick={() => setOpen(false)} className={navigationMenuTriggerStyle()}>الخدمات</NavigationMenuLink>
+                                        </Link>
+                                    </NavigationMenuItem>
+                                    <NavigationMenuItem>
+                                        <Link href="/#testimonials" legacyBehavior passHref>
+                                            <NavigationMenuLink onClick={() => setOpen(false)} className={navigationMenuTriggerStyle()}>العملاء</NavigationMenuLink>
+                                        </Link>
+                                    </NavigationMenuItem>
+                                    <NavigationMenuItem>
+                                        <Link href="/#about" legacyBehavior passHref>
+                                            <NavigationMenuLink onClick={() => setOpen(false)} className={navigationMenuTriggerStyle()}>من نحن</NavigationMenuLink>
+                                        </Link>
+                                    </NavigationMenuItem>
+                                </NavigationMenuList>
+                            </NavigationMenu>
+                        </div>
+                    </PopoverContent>
+                </Popover>
+            </div>
+        </header>
+    )
+}
