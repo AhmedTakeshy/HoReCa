9285df69faeaa7421999870901517d763efd0ffe
Ahmed Takeshy <ahmedtakeshy7@gmail.com>
2023-12-28 14:50:39 +0300

    Fix typos and formatting in DUMMYDATA.ts

--- src/lib/DUMMYDATA.ts
+++ src/lib/DUMMYDATA.ts
@@ -7,29 +7,29 @@ export const dataList: List[] = [
     {
         header: "Home, Garden, DIY Market",
         data: {
-            "Home & Kitchen Appliances": ["Dowry set", "Disposable Household Products", "Decoration", "Bathroom accessories", "Table, Service"],
+            "Home Kitchen Appliances": ["Dowry set", "Disposable Household Products", "Decoration", "Bathroom accessories", "Table, Service"],
             "Pet Products": ["Dog", "Reptile", "Rodent", "Bird", "Fish", "Cat"],
             "Electrical installations": ["Extension Cable, Group Socket", "Fly Killing Device", "Cable", "Installation materials", "LED Lighting", "Island, Button, Automatic"],
-            "Workwear & Safety": ["Women's Clothing", "Men's clothing", "Traffic Safety"],
-            "Mixer, Sink": ["Sink, Sink Mixer", "Bath & Shower Faucet", "Faucet"],
+            "Work wear, Safety": ["Women's Clothing", "Men's clothing", "Traffic Safety"],
+            "Mixer, Sink": ["Sink, Sink Mixer", "Bath  Shower Faucet", "Faucet"],
             "Garden": ["Flower, Plant", "Irrigation", "Grill, Barbecue", "Garden Decoration", "Garden furniture", "Garden Tools"],
-            "Shower Systems, Sanitaryware": ["Shower", "Bath & Shower Faucet", "Bathtub, Jacuzzi, Boat", "Installation materials", "Bathroom accessories", "Ventilation", "Sanitary ware"],
-            "Construction materials": ["Construction Materials", "Silicone & Silicone Gun", "Isolation", "Door, Window", "Sheets", "Material Handling"]
+            "Shower Systems, Sanitary ware": ["Shower", "Bath  Shower Faucet", "Bathtub, Jacuzzi, Boat", "Installation materials", "Bathroom accessories", "Ventilation", "Sanitary ware"],
+            "Construction materials": ["Construction Materials", "Silicone  Silicone Gun", "Isolation", "Door, Window", "Sheets", "Material Handling"]
         },
     },
     {
         header: "Health, Cleaning, Cosmetics", 
         data: {
-            "Baby Health & Care": ["Breastfeeding", "Baby Nutrition Products", "Baby Diapers, Wet Wipes", "Baby Bath & Toilet", "Baby Health", "Baby care"],
-            "Personal care": ["Lip Makeup", "Hand Care", "Bathroom, Toilet", "Body care", "Oral & Dental Care", "Sun Care"],
-            "Cleaning": ["Paper Products", "Cleaning Dispenser", "Room Fragrance", "Incense", "Industrial Cleaner & Detergent", "Cleaning Supplies", "Household Cleaner & Detergent"],
+            "Baby Health Care": ["Breastfeeding", "Baby Nutrition Products", "Baby Diapers, Wet Wipes", "Baby Bath  Toilet", "Baby Health", "Baby care"],
+            "Personal care": ["Lip Makeup", "Hand Care", "Bathroom, Toilet", "Body care", "Oral  Dental Care", "Sun Care"],
+            "Cleaning": ["Paper Products", "Cleaning Dispenser", "Room Fragrance", "Incense", "Industrial Cleaner  Detergent", "Cleaning Supplies", "Household Cleaner  Detergent"],
             "Health, Medical": ["Disposable Health Products", "Diagnostics, Measurement", "Health products", "Orthopedic Products", "Nutritional Supplement", "Physiotherapy Products"]
         }
     },
     {
         header: "Jewelry, Shoe, Bag", 
         data: {
-            "Suitcase": ["Cabin Size Suitcase", "Large Suitcase", "Travel bag", "Luggage Set", "Bag & Luggage Accessories", "Medium Size Suitcase"],
+            "Suitcase": ["Cabin Size Suitcase", "Large Suitcase", "Travel bag", "Luggage Set", "Bag  Luggage Accessories", "Medium Size Suitcase"],
             "Shoe": ["Shoe Materials", "Men's shoes", "Children's Shoes", "Women's Shoes", "Shoe Accessories"],
             "Bag, Wallet": ["Women Bag", "Wallet, Card Holder", "School Bag, Water Bottle", "Promotional Bag", "Men's Bag", "Shopping bag"],
         }
@@ -45,7 +45,7 @@ export const dataList: List[] = [
         data: {
             "Stationery": ["Notebook", "Stationery Set", "School Bag, Water Bottle", "School Supplies", "Office Supplies", "Office Furniture", "Office Machines"],
             "Hobby": ["Hobby Materials", "Hobby Tools", "Hobby Accessories", "Hobby Toys", "Hobby Books", "Hobby Paints", "Hobby Paints"],
-            "Book": ["Book", "Book Accessories", "Book & Magazine", "Book & Magazine", "Book & Magazine", "Book & Magazine", "Book & Magazine"]
+            "Book": ["Book", "Book Accessories", "Book  Magazine", "Book  Magazine", "Book  Magazine", "Book  Magazine", "Book  Magazine"]
         }
     },
     {
