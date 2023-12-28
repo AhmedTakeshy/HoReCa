39a4609cffa2d48a30473e74295803233cd22134
Ahmed Takeshy <ahmedtakeshy7@gmail.com>
2023-12-28 14:50:44 +0300

    Update types.d.ts with new Product and Cart interfaces

--- types.d.ts
+++ types.d.ts
@@ -1,16 +1,24 @@
-interface Product {
+type Product ={
     id: number,
-    name: string,
+    title: string,
     price: number,
-    image: string,
+    thumbnail: string,
+    image: string[],
+    brand: string,
+    rating: number,
     description: string,
     category: string,
     stock: number,
     quantity: number,
 }
 
-interface Cart {
+type Cart ={
     items:Product[],
     totalAmount: number,
     totalQuantity: number,
 }
+
+type ServerResponse<T> =
+    | { status: "loading" }
+    | { data: T; status: "success"; statusCode: number }
+    | { error: string; status: "error"; statusCode: number };
