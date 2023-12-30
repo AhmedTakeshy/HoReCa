439fd406a8db27bb6202f151693e9d0e060bc0b9
Ahmed Takeshy <ahmedtakeshy7@gmail.com>
2023-12-30 17:29:37 +0300

    Add loading component to home page

---
+++ src/app/(home)/loading.tsx
@@ -0,0 +1,12 @@
+import LoadingBanner from '@/components/home/LoadingBanner'
+import LoadingCard from '@/components/home/LoadingCard'
+import React from 'react'
+
+export default function loading() {
+    return (
+        <>
+        <LoadingBanner />
+        <LoadingCard />
+        </>
+    )
+}
