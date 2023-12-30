6b961c65877f9ea762170c663e0f7236de51a8b2
Ahmed Takeshy <ahmedtakeshy7@gmail.com>
2023-12-30 17:30:05 +0300

    Add ActionButton component

---
+++ src/components/shared/ActionButton.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useAppDispatch } from "@/_store/hooks"
+import { Button, ButtonProps } from "../ui/button"
+import { addToCart, removeFromCart } from "@/_store/cartSlice"
+import { toast } from "sonner"
+
+type Props = ButtonProps & {
+    children: string | React.ReactNode
+    data?: Product
+}
+
+export default function ActionButton({ children, data, ...props }: Props) {
+    const dispatch = useAppDispatch()
+    const addProduct = () => {
+        if (data) {
+            dispatch(addToCart(data))
+            toast(`Added Item`, {
+                description: `${data.title} has been added to the cart`,
+                action: {
+                    label: "Undo",
+                    onClick: () => dispatch(removeFromCart(data.id)),
+                },
+            })
+        }
+    }
+    return (
+        <Button {...props} onClick={addProduct}>
+            {children}
+        </Button>
+    )
+}
