5aebaf310e7ca9791eaf2db6c7e7b6f5837771bd
Ahmed Takeshy <ahmedtakeshy7@gmail.com>
2023-12-30 17:30:36 +0300

    Add CDN for dummyjson.com

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
                 protocol: "https",
                 hostname: "n11scdn.akamaized.net",
             },
+            {
+                protocol: "https",
+                hostname: "i.dummyjson.com",
+            },
         ],
     }
 }
