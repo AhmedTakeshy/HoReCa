31845719893b12bceeede78f1033e7a8f4f83bd2
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-05-06 01:59:00 +0300

    chore: Add new pages for cart and signup, and update Sentry example page

---
+++ src/app/(home)/cart/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cart from './_components/cart'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
+import { getCartFromDatabase } from '@/_actions/cartActions'
+
+export default async function page() {
+    const session = await getServerSession(authOptions)
+    const cartResponse = session?.user.email ? await getCartFromDatabase(session.user.email) : undefined
+
+    return (
+        <div className='flex flex-col gap-4'>
+            <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
+            <Cart cartDb={cartResponse?.status === "Success" ? cartResponse.data : undefined} user={session?.user} />
+        </div>
+    )
+}
---
+++ src/app/(home)/signup/loading.tsx
@@ -0,0 +1,7 @@
+import LoadingAuth from "@/app/(home)/_components/LoadingAuth";
+
+export default function loading() {
+    return (
+        <LoadingAuth />
+    )
+}
