6f8741402054cfbabc5a99224619a1ab5fd71f1a
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-05-06 01:56:11 +0300

    Add new files and components for authentication

---
+++ src/app/(home)/_components/LoadingAuth.tsx
@@ -0,0 +1,8 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+
+export default function LoadingAuth() {
+    return (
+        <Skeleton className=" w-[30rem] h-[22rem] rounded-md" />
+    )
+}
--- src/app/(home)/_components/LoadingBanner.tsx
+++ src/app/(home)/_components/LoadingBanner.tsx
@@ -1,7 +1,4 @@
-import Link from 'next/link';
-import React from 'react'
 import { Skeleton } from "../../../components/ui/skeleton";
-import { MdOutlinePhoneAndroid } from 'react-icons/md';
 
 export default function LoadingBanner() {
 
