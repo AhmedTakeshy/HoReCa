5200bb4fa2fcef1371a65f040184cd24ed12b19b
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-07-04 23:53:35 +0300

    refactor: Update package.json and dependencies
    This commit updates the package.json file to change the project name from "nova_boutique" to "horeca". It also updates various dependencies to their latest versions, including "@hookform/resolvers", "@prisma/client", "@radix-ui/react-dropdown-menu", "@radix-ui/react-hover-card", "@radix-ui/react-label", "@radix-ui/react-navigation-menu", "@radix-ui/react-popover", "@radix-ui/react-radio-group", "@radix-ui/react-select", "@radix-ui/react-slider", "@radix-ui/react-slot", "@radix-ui/react-tabs", "@reduxjs/toolkit", "@sentry/nextjs", "@stripe/stripe-js", "@tsparticles/engine", "@tsparticles/slim", "embla-carousel-autoplay", "embla-carousel-react", "framer-motion", "react-hook-form", "stripe", and "tailwindcss-animate".
    
    These changes improve the project's functionality, performance, and compatibility with the latest versions of the dependencies.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from '@prisma/client'
-export const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+export const prisma = new PrismaClient().$extends(withAccelerate())
+
 
 
